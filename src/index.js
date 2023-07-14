717bf6bfe664797ca81dba17d7714143b49a28a2
KiteKite <kite.dtk@gnail.com>
2023-07-14 10:46:23 +0700

    Static Files & SCSS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 // Http logger middleware
 const morgan = require('morgan');
 app.use(morgan('combined'));
