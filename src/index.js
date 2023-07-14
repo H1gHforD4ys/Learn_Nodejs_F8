45a51e4ab924645ccdefac653c3b6ef7b7bc2c6f
KiteKite <kite.dtk@gnail.com>
2023-07-14 10:08:00 +0700

    Template Engine HandleBars

---
+++ src/index.js
@@ -0,0 +1,27 @@
+var path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Http logger middleware
+const morgan = require('morgan');
+app.use(morgan('combined'));
+
+// Template engine setup
+const { engine } = require('express-handlebars');
+app.engine('hbs', engine({ extname: '.hbs' })); //Định nghĩa đuôi mở rộng
+
+app.set('view engine', 'hbs');
+app.set('views', './src/resources/views');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+    
+    res.render('home');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
