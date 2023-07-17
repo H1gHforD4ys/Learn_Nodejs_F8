8b7b24391904711c0e46ab86e9f162e1670313e8
KiteKite <kite.dtk@gnail.com>
2023-07-17 14:51:18 +0700

    render home and mapping value to object

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,17 @@
 const Course = require('../models/Course'); 
+const { multipleMongooseToObject } /*Viết tnay để lấy hàm bên trong(phải để đúng tên hàm)*/= require('../../util/mongoose');
 //Viết controller ở đây
 class SiteController{
     //[get] // trả về trang chủ
     index(req, res, next){
-        Course.find({}) // Dưới dạng promise
-            .then(course => res.json(course)) // thành công thì render về course nhận được
-            .catch(next); 
-        
-        //res.render('home'); // render lại file news trong views
+        Course.find({/* trong này là điều kiện truy vẫn */}) // Dưới dạng promise
+            .then(courses =>{
+                res.render('home',{
+                    courses : multipleMongooseToObject(courses)
+                });
+            }) // thành công thì render về course nhận được
+            .catch(next);
+            //Lấy tất cả các dữ liệu của course
     };
 
     //[get]/search  / trang tìm kiếm
